8bd34e6c2f52fe6c386d789d5ca07a6c53807eb3
maria cristina gallego herrero <mariacg@zeta40.aulas.gsyc.es>
2016-02-02 13:01:10 +0100

    Ejercicio clase

--- ejercicio.py
+++ ejercicio.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+fichero = open("/etc/passwd", "r")
 
+while 1:
+    lineas = fichero.readline()
+    user = lineas.split(":")
+    print user[0], user[-1][:-1]
+    if not lineas:
+         break
+
+fichero.close()
+fichero = open("/etc/passwd", "r")
+num_lineas = fichero.readlines()
+print "Hay", len(num_lineas), "usuarios"
+
+
+fichero.close()
